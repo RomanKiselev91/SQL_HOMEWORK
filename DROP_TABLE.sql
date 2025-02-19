ec832cf8cc92a6a9e511eb31374888529d51b657
RomanKiselev91 <www.ksel@gmail.com>
2025-02-19 21:28:41 +0300

    Add files via upload

---
+++ DROP_TABLE.sql
@@ -0,0 +1,9 @@
+DROP TABLE track_artist;
+DROP TABLE artist_album;
+DROP TABLE genres_artist;
+DROP TABLE album_track;
+DROP TABLE artist;
+DROP TABLE album;
+DROP TABLE genres;
+DROP TABLE track;
+DROP TABLE collection;
